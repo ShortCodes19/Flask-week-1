d038b115b4d4070278be8673811f70ea125dffd1
JK <jumak4863@gmail.com>
2025-09-12 17:05:31 +0500

    new

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+import pytz
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///post.db'
@@ -8,10 +10,17 @@ app.config['SECRET_KEY'] = 'somethingsecret'
 
 db = SQLAlchemy(app)
 
+@app.template_filter('localtime')
+def localtime_filter(utc_datetime):
+    local_tz = pytz.timezone('Asia/Karachi')
+    local_datetime = utc_datetime.astimezone(local_tz)
+    return local_datetime.strftime('%Y-%m-%d %H:%M:%S')
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
 
 @app.route('/')
 def home():
@@ -27,8 +36,9 @@ def add():
     if request.method == 'POST':
         title = request.form.get('title')
         content = request.form.get('content')
+        timestamp = datetime.now(timezone.utc)
 
-        new_post = Post(title=title, content=content)
+        new_post = Post(title=title, content=content, timestamp=timestamp)
         db.session.add(new_post)
         db.session.commit()
         return redirect(url_for('view_all'))
@@ -49,6 +59,20 @@ def edit(id):
         db.session.commit()
         return redirect(url_for('view_all'))
     return render_template('edit.html', post=post)
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete(id):
+    post = Post.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('view_all'))
+
+@app.route('/test_timezone')
+def test_timezone():
+    utc_now = datetime.now(timezone.utc)
+    local_tz = pytz.timezone('Asia/Karachi')
+    local_now = utc_now.astimezone(local_tz)
+    return render_template('test_timezone.html', utc_now=utc_now, local_now=local_now)
 
 if __name__ == '__main__':
     with app.app_context():
