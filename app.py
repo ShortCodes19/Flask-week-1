09eef91797e829cb4d73d423ea6d3e27056a5095
JK <jumak4863@gmail.com>
2025-09-13 07:36:01 +0500

    new

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///post.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secret'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
 
 @app.route("/")
 def hello_world():
     return render_template('base.html')
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        new_post = Post(title=title, content=content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('view_all'))
+    return render_template('add.html')
+
+@app.route('/view_all')
+def view_all():
+    posts = Post.query.all()
+    return render_template('view_all.html', posts=posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
